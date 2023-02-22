22eae364cbd6de80f8acbdadc8e9bbca5f810036
salah24 <salahcyb@gmail.com>
2023-02-22 12:36:40 +0100

    create edit note view

---
+++ lib/views/edite_note_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditeNoteViewBody extends StatelessWidget {
+  const EditeNoteViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,47 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: const Color(0xffffcc80),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text('Build your career with Hocine',
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(.4), fontSize: 18)),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return EditeNoteView();
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xffffcc80),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text('Build your career with Hocine',
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(.4), fontSize: 18)),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May 21 2022',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May 21 2022',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.4), fontSize: 16),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
